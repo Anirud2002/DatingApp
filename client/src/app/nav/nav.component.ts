4a16d7a8afb737837ee6834b5db1e8dc592a7ff2
Anirud Shrestha <anirudshrestha@boisestate.edu>
2022-07-11 08:16:06 -0600

    end of section 7

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -25,9 +25,6 @@ export class NavComponent implements OnInit {
     this.accountService.login(this.model).subscribe({
       next: (res) => {
         this.router.navigateByUrl('/members')
-      },
-      error: err => {
-        this.toastr.error(err.error)
       }
     })
   }

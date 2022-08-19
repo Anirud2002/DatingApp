6c20d6f0049d6f21e23570705deabe67a0bb82e1
Anirud Shrestha <anirudshrestha@boisestate.edu>
2022-08-19 15:01:50 -0600

    Adding state mgmt using services

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map, of } from 'rxjs';
 import { environment } from '../../environments/environment';
 import { Member } from '../_models/members';
 
@@ -9,18 +10,32 @@ import { Member } from '../_models/members';
 })
 export class MembersService {
   apiBaseUrl = environment.apiBaseUrl;
+  members: Member[] = [];
 
   constructor(private http: HttpClient) { }
 
   getMembers(){
-    return this.http.get<Member[]>(this.apiBaseUrl + "users");
+    if(this.members.length > 0) return of(this.members)
+    return this.http.get<Member[]>(this.apiBaseUrl + "users").pipe(
+      map(members => {
+        this.members = members
+        return members;
+      })
+    )
   }
 
   getMember(username: string){
+    const member = this.members.find(member => member.username === username);
+    if(member !== undefined) return of(member)
     return this.http.get<Member>(this.apiBaseUrl + "users/" + username);
   }
 
   updateMember(member: Member){
-    return this.http.put(this.apiBaseUrl + 'users/', member)
+    return this.http.put(this.apiBaseUrl + 'users/', member).pipe(
+      map(() => {
+        const index = this.members.indexOf(member);
+        this.members[index] = member;
+      })
+    )
   }
 }
